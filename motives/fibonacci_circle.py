6dccbc283019c933974703ea0345e4c2103dcfe8
karlwen95 <karlwen@student.chalmers.se>
2025-08-13 23:20:54 +0200

    Creating figure

--- motives/fibonacci_circle.py
+++ motives/fibonacci_circle.py
@@ -14,6 +14,7 @@ Motive
 # Imports
 import numpy as np
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 
 # The fibonacci series as a generator
@@ -42,7 +43,38 @@ def leading_three_digits(num: int):
     return f_num
 
 
+def get_circle_position(num: float):
+    """
+    Find the coordinates for a number between 0-10 on the circle with radius 1.
+    Starting from the positive x-axis going counterclockwise.
+    :param num: float | Three digit number between 0 and 10.
+    :return: x- and y-coordinates on the circle
+    """
+    # Convert the number to radians
+    max_num = 10.0
+    num = num * (2 * np.pi) / max_num
+    x, y = np.cos(num), np.sin(num)
+    return x, y
+
+
 if __name__ == '__main__':
-    fib_generator = fibonacci_sequence(100)
-    for n in fib_generator:
-        print(leading_three_digits(n))
+    n = 1000
+    fib_generator = fibonacci_sequence(n)
+
+    xs, ys = [], []
+    for num in fib_generator:
+        scaled_num = leading_three_digits(num)
+        x,y = get_circle_position(scaled_num)
+        xs.append(x)
+        ys.append(y)
+        #print(f'Number: {num}')
+        #print(f'Coords: {x, y}')
+
+    plt.figure()
+    # Create circle
+    x = np.linspace(0, 2*np.pi, 1000)
+    plt.plot(np.cos(x), np.sin(x), linewidth=0.1, c='k')
+    plt.plot(xs, ys, linewidth=0.05, c='y')
+    plt.title(f'Fibonacci circle with n={n}')
+    plt.axis('scaled')  # Equally scaled axes
+    plt.show()
